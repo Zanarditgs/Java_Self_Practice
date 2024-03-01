f7c6d35b437a2418e16cc6a2b972ad8d8fa2c2dd
zanarditgs <turkishairlinescrew@gmail.com>
2024-03-01 17:51:53 -0500

    PracticeTasks

---
+++ src/question_of_the_day/IfAndElseStatement.java
@@ -0,0 +1,13 @@
+package question_of_the_day;
+
+public class IfAndElseStatement {
+    public static void main(String[] args) {
+        int score = 75;
+        if (score >= 60){
+            System.out.println("Congruculation! You Passed the Exam");
+        }else {
+            System.out.println("Sorry ! You Failed the Exam");
+        }
+    }
+
+}
